4795045b3a1c3cbc39132054662e2251a5f5a505
ashutoshmalode <ashutosh.malodeofficial@outlook.com>
2026-06-27 14:47:47 +0530

    production first changes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
