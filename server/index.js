d381bb06d7e7ef0fb7435d19a214b29058d19397
ashutoshmalode <ashutosh.malodeofficial@outlook.com>
2026-06-27 15:11:59 +0530

    added latest domain

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://classlens-smart-attendance-hqpe.vercel.app/",
+      "https://classlens-smart-attendance.vercel.app/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
